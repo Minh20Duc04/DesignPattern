01eb3fe6b797a1139986b01a48942b8061c58e1e
Minh20Duc04 <nguyenminhduc02042004@gmail.com>
2025-05-29 15:47:24 +0700

    Finish test

--- DesignPattern/AbstractFactory/src/main/java/Car/testGit2.java
+++ DesignPattern/AbstractFactory/src/main/java/Car/testGit2.java
@@ -2,6 +2,6 @@ package Car;
 
 public class testGit2 {
     public static void main(String[] args) {
-        System.out.println("This is Minh Duc");
+        System.out.println("This is Minh Duc and his Collaborator");
     }
 }
